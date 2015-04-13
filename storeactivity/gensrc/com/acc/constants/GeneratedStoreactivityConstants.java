e7cc4cda892633e9917d3151cc392a994cff44aa
swarnimagupta <swarniima@gmail.com>
2015-04-13 17:45:21 +0530

    new addon for store activity

---
+++ storeactivity/gensrc/com/acc/constants/GeneratedStoreactivityConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Apr 13, 2015 4:14:36 PM                     ---
+ * ----------------------------------------------------------------
+ */
+package com.acc.constants;
+
+/**
+ * @deprecated use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedStoreactivityConstants
+{
+	public static final String EXTENSIONNAME = "storeactivity";
+	
+	protected GeneratedStoreactivityConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
